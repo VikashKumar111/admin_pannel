ae371dbf8dcaff79e743f339d3b7fd595a7a1c18
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-22 13:27:52 +0530

    add

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,9 +5,18 @@ import "./Login.css"; // Ensure the path is correct
 const Login = () => {
   return (
     <div className="login-container">
+      <br />
+      <br />
       <div className="login-form-container">
-        <CustomInput type="text" label="Email" id="email" />
+        <h3 className="text-center">Login</h3>
+        <p className="text-center">Login to your account to continue</p>
+        <form action="">
+          <CustomInput type="text" label="Email" id="email" />
         <CustomInput type="text" label="Password" id="pass" />
+        <button className="login-button" type="submit">
+          Login
+        </button>
+        </form>
       </div>
     </div>
   );
