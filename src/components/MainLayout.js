d1e6b36fb276ec36081e9a29d7aa906fbe250c5c
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-24 11:29:22 +0530

    add

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,41 +1,160 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
   UploadOutlined,
   UserOutlined,
   VideoCameraOutlined,
-} from '@ant-design/icons';
-import { Button, Layout, Menu, theme } from 'antd';
+} from "@ant-design/icons";
+import {
+  AiOutlineDashboard,
+  AiOutlineShoppingCart,
+  AiOutlineUser,
+  AiOutlineBgColors,
+} from "react-icons/ai";
+import { MdDashboard } from "react-icons/md";
+import { SiBrandfolder } from "react-icons/si";
+import { BiCategoryAlt } from "react-icons/bi";
+import { ImBlog } from "react-icons/im";
+import { FaClipboardList, FaBloggerB } from "react-icons/fa";
+import { RiCouponLine } from "react-icons/ri";
+import { Button, Layout, Menu, theme } from "antd";
+import { useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 const { Header, Sider, Content } = Layout;
 const MainLayout = () => {
   const [collapsed, setCollapsed] = useState(false);
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
+  const navigate = useNavigate();
   return (
     <Layout>
       <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo" />
+        <div className="logo">
+          <h2 className="text-white fs-5 text-center py-3 mb-0">
+            <span className="sm-logo">LOGO</span>
+            <span className="lg-logo">BME</span>
+          </h2>
+        </div>
         <Menu
           theme="dark"
           mode="inline"
-          defaultSelectedKeys={['1']}
+          defaultSelectedKeys={[""]}
+          onClick={({ key }) => {
+            navigate(key);
+          }}
           items={[
             {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
+              key: "",
+              icon: <MdDashboard className="fs-5" />,
+              label: "Dashboard",
             },
             {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
+              key: "customers",
+              icon: <AiOutlineUser className="fs-4" />,
+              label: "Customers",
             },
             {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
+              key: "Catalog",
+              icon: <AiOutlineShoppingCart className="fs-4" />,
+              label: "Catalog",
+              children: [
+                {
+                  key: "product",
+                  icon: <AiOutlineShoppingCart className="fs-4" />,
+                  label: "Add Product",
+                },
+                {
+                  key: "list-product",
+                  icon: <AiOutlineShoppingCart className="fs-4" />,
+                  label: "Product List",
+                },
+                {
+                  key: "brand",
+                  icon: <SiBrandfolder className="fs-4" />,
+                  label: "Brand",
+                },
+                {
+                  key: "list-brand",
+                  icon: <SiBrandfolder className="fs-4" />,
+                  label: "Brand List ",
+                },
+                {
+                  key: "category",
+                  icon: <BiCategoryAlt className="fs-4" />,
+                  label: "Category",
+                },
+                {
+                  key: "list-category",
+                  icon: <BiCategoryAlt className="fs-4" />,
+                  label: "Category List",
+                },
+                {
+                  key: "color",
+                  icon: <AiOutlineBgColors className="fs-4" />,
+                  label: "Color",
+                },
+                {
+                  key: "list-color",
+                  icon: <AiOutlineBgColors className="fs-4" />,
+                  label: "Color List",
+                },
+              ],
+            },
+            {
+              key: "orders",
+              icon: <FaClipboardList className="fs-4" />,
+              label: "Orders",
+            },
+            {
+              key: "marketing",
+              icon: <RiCouponLine className="fs-4" />,
+              label: "Marketing",
+              children: [
+                {
+                  key: "coupon",
+                  icon: <ImBlog className="fs-4" />,
+                  label: "Add Coupon",
+                },
+                {
+                  key: "coupon-list",
+                  icon: <RiCouponLine className="fs-4" />,
+                  label: "Coupon List",
+                },
+              ],
+            },
+            {
+              key: "blogs",
+              icon: <FaBloggerB className="fs-4" />,
+              label: "Blogs",
+              children: [
+                {
+                  key: "blog",
+                  icon: <ImBlog className="fs-4" />,
+                  label: "Add Blog",
+                },
+                {
+                  key: "blog-list",
+                  icon: <FaBloggerB className="fs-4" />,
+                  label: "Blog List",
+                },
+                {
+                  key: "blog-category",
+                  icon: <ImBlog className="fs-4" />,
+                  label: "Add Blog Category",
+                },
+                {
+                  key: "blog-category-list",
+                  icon: <FaBloggerB className="fs-4" />,
+                  label: "Blog Category List",
+                },
+              ],
+            },
+            {
+              key: "enquiries",
+              icon: <FaClipboardList className="fs-4" />,
+              label: "Enquiries",
             },
           ]}
         />
@@ -52,7 +171,7 @@ const MainLayout = () => {
             icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
             onClick={() => setCollapsed(!collapsed)}
             style={{
-              fontSize: '16px',
+              fontSize: "16px",
               width: 64,
               height: 64,
             }}
@@ -60,14 +179,14 @@ const MainLayout = () => {
         </Header>
         <Content
           style={{
-            margin: '24px 16px',
+            margin: "24px 16px",
             padding: 24,
             minHeight: 280,
             background: colorBgContainer,
             borderRadius: borderRadiusLG,
           }}
         >
-          Content
+          <Outlet />
         </Content>
       </Layout>
     </Layout>
